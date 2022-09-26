b8ae100355aa60cdff04e4dbe068b79904f9bff6
Hitesh028 <55538188+Hitesh028@users.noreply.github.com>
2022-09-26 15:39:32 +0530

    Create Software running on Raspberry pi server.c

---
+++ Software running on Raspberry pi server.c
@@ -0,0 +1,7 @@
+//Importing all these libraries json, csv, datetime, paho.mqtt.client, mysql.connector, base64, hashlib, Cryptodome
+//Define  function for base64 and aes256 encryption
+//Give a long password for AES256 encryption
+//Define function for MySQL database connection to store tag data.
+//The callback for when the client receives a CONNACK response from the server.
+//The callback for when a PUBLISH message is received from the server.
+//Listen on port for 60 seconds
